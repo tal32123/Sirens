4c9396761eece349b9e048df31b7462cc0e5fc22
Tal <tal32123@gmail.com>
2018-12-22 23:55:31 +0200

    fix: bugs

--- Client/scripts/app.js
+++ Client/scripts/app.js
@@ -1,39 +1,38 @@
-(function(){
+(function () {
 
-var app = angular.module("myapp", []);
+    var app = angular.module("myapp", []);
 
-app.controller("sirenController", ($scope) => {
-    $scope.sirensArray = [];
+    app.controller("sirenController", ($scope) => {
+        $scope.sirensArray = [];
 
-  var siren = {
-   description: $scope.issueName,
-   severity: $scope.severity,
-   start: $scope.issueDate,
-   end: $scope.issueEnd,
-   id: chance.guid(),
-  };
+        var siren = {
+            description: $scope.issueName,
+            severity: $scope.severity,
+            start: $scope.issueDate,
+            end: $scope.issueEnd,
+            id: chance.guid(),
+        };
 
-$scope.severityLevels = [1, 2, 3, 4, 5];
+        $scope.severityLevels = [1, 2, 3, 4, 5];
 
-    $scope.siren= siren;
-
-
-  $scope.submit = function() {
-    var identity = chance.guid();
-    var submittedSiren = {
-     description: $scope.issueName,
-      severity: $scope.severity,
-     start: $scope.issueDate,
-      end: $scope.issueEnd,
-      id: identity,
-    };
-    console.log($scope.sirensArray);
-    $scope.sirensArray.push(submittedSiren);
-      // $scope.sirensArray.push($scope.siren);
-  }
+        $scope.siren = siren;
 
 
-});
+        $scope.submit = function () {
+            var identity = chance.guid();
+            var submittedSiren = {
+                description: $scope.issueName,
+                severity: $scope.severity,
+                start: $scope.issueDate,
+                end: $scope.issueEnd,
+                id: identity,
+            };
+            console.log($scope.sirensArray);
+            $scope.sirensArray.push(submittedSiren);
+        }
+
+
+    });
 
 
 }());
